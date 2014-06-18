72e1df933d9449c166b3a042485b280b2ebf95a1
anton <wgalaev@gmail.com>
2014-06-18 18:36:22 +0400

    index page shows real info

--- src/main/webapp/resources/js/bugs.js
+++ src/main/webapp/resources/js/bugs.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $("#showclosed").prop("disabled", false);
     $("#showclosed").click(function(){
         var url = location.protocol + '//' + location.host + location.pathname + "?";
         var query = window.location.search.substring(1);
@@ -17,6 +18,7 @@ $(document).ready(function(){
                 url = url + pair[0] + "=" + pair[1];
             }
         }
+        $("#showclosed").prop("disabled", true);
         location.href = url;
     });
 });
